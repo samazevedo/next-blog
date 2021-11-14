3ca7fd9219f5d2dc1924cf949b7a865ec2be5a99
Sam Azevedo <34683893+samazevedo@users.noreply.github.com>
2021-11-14 06:29:16 -0300

    change styles and colors

--- components/Layouts/Logo/Logo.component.tsx
+++ components/Layouts/Logo/Logo.component.tsx
@@ -1,5 +1,9 @@
 import styles from './Logo.module.css'
 
 export const Logo = () => {
-    return <div className={styles.logo}>Mia</div>
+    return (
+        <div className={styles.logo}>
+            <h1>Mia</h1>
+        </div>
+    )
 }
