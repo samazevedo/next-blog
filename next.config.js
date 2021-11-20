8499c802ffccfab79e052d9f67b924dff63f0ef8
Sam Azevedo <34683893+samazevedo@users.noreply.github.com>
2021-11-20 02:47:45 -0300

    fixed issue

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    env: {
+        mongodb_username: 'sam-azev00',
+        mongodb_password: 'S7WBrUEYcoGIjkUb',
+        mongodb_clutername: 'cluster0',
+        mongodb_database: 'nextBlogMIA',
+    },
+}
