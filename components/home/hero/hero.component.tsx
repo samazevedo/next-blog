29e40056a65ec63333b892e7437ac003c13eb53e
Sam Azevedo <34683893+samazevedo@users.noreply.github.com>
2021-11-20 03:16:53 -0300

    fix hero page again

--- components/home/hero/hero.component.tsx
+++ components/home/hero/hero.component.tsx
@@ -12,9 +12,13 @@ export const Hero = () => {
                     height={350}
                 />
             </div>
-            <h1>Hi, I'm Mia</h1>
-            <p>Welcome to blog!</p>
-            <p>I am a blogger and a developer.</p>
+            <h1>Hello, I am Mia</h1>
+            <h2>Welcome to blog!</h2>
+            <p>I am a blogger and web developer.</p>
+            <p>
+                This blog I share my thoughts and experiences about web
+                development and programming.
+            </p>
         </section>
     )
 }
